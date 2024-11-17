2724c4483624508a776ef26c272b942dc69256f1
Andrii Zaitsev <andreyzaitsev31@gmail.com>
2024-11-17 23:59:09 +0200

    Rename Tickers to Symbols
    - _symbol_ is more correct for this industry

--- src/components/Tickers/index.tsx
+++ src/components/Tickers/index.tsx
@@ -1,19 +1,19 @@
 import { useRecoilValue } from "recoil";
 import { Stack, List } from "@mui/material";
-import TickerListItem from "./TickerListItem";
+import SymbolListItem from "./TickerListItem";
 import tickersState from "@/state/tickers";
 
-const Tickers = () => {
+const Symbols = () => {
   const symbols = useRecoilValue(tickersState);
   return (
     <Stack>
       <List>
         {symbols.map((symbol) => (
-          <TickerListItem ticker={symbol} key={symbol.id} />
+          <SymbolListItem ticker={symbol} key={symbol.id} />
         ))}
       </List>
     </Stack>
   );
 };
 
-export default Tickers;
+export default Symbols;
