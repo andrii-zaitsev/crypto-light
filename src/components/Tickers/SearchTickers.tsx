764b8c21a88ea44941aa76cb660fcd10a5cb73f9
Andrii Zaitsev <andreyzaitsev31@gmail.com>
2024-11-27 08:51:43 +0200

    Turn search tickers list into virtual list for performance

--- src/components/Tickers/SearchTickers.tsx
+++ src/components/Tickers/SearchTickers.tsx
@@ -1,6 +1,7 @@
-import { useMemo } from "react";
+import { useMemo, useRef } from "react";
 import { useRecoilValue, useRecoilState, useSetRecoilState } from "recoil";
 import { useQuery } from "@tanstack/react-query";
+import { useVirtualizer } from "@tanstack/react-virtual";
 import {
   List,
   ListItem,
@@ -36,89 +37,131 @@ const SearchTickers = () => {
     [data, search]
   );
 
+  const parentRef = useRef(null);
+
+  const virtualizer = useVirtualizer({
+    count: searchTickers.length,
+    getScrollElement: () => parentRef.current,
+    estimateSize: () => 72,
+    overscan: 5
+  });
+
+  console.log({ searchTickersL: searchTickers.length });
+
   return (
-    <div>
-      <List>
-        {searchTickers.map((ticker) => {
-          const percentChange = Number(ticker.changePercent24Hr);
-          const isGrowth = percentChange === Math.abs(percentChange);
-          const isSelected = !!tickers.find(
-            (savedTicker) => savedTicker.id === ticker.id
-          );
-          return (
-            <ListItem
-              key={ticker.id}
-              sx={{
-                paddingLeft: 0,
-                "&:hover": { backgroundColor: "rgba(0, 0, 0, 0.04)" }
-              }}
-            >
-              <ListItemIcon>
-                {isSelected ? (
-                  <IconButton
-                    onClick={() =>
-                      setTickers((prevTickers) =>
-                        prevTickers.filter(
-                          (savedTicker) => savedTicker.id !== ticker.id
-                        )
-                      )
-                    }
-                    sx={{ paddingLeft: 0 }}
-                  >
-                    <CheckCircleIcon color="primary" fontSize="large" />
-                  </IconButton>
-                ) : (
-                  <IconButton
-                    onClick={() =>
-                      setTickers((prevTickers) => [
-                        ...prevTickers,
-                        { id: ticker.id, value: ticker.symbol }
-                      ])
-                    }
-                    sx={{ paddingLeft: 0 }}
-                  >
-                    <ControlPointIcon color="primary" fontSize="large" />
-                  </IconButton>
-                )}
-              </ListItemIcon>
-              <Box
-                display="flex"
-                alignItems="center"
-                sx={{ width: "100%" }}
-                onClick={() =>
-                  selectTicker({ id: ticker.id, value: ticker.symbol })
-                }
+    <Box
+      sx={{
+        height: `${window.innerHeight - 100}px`,
+        width: `100%`,
+        overflow: "auto"
+      }}
+      ref={parentRef}
+    >
+      {searchTickers.length === 0 && (
+        <Typography>{`No results for "${search}"`}</Typography>
+      )}
+      {!!searchTickers.length && (
+        <List
+          sx={{
+            height: `${virtualizer.getTotalSize()}px`,
+            width: "100%",
+            position: "relative"
+          }}
+        >
+          {virtualizer.getVirtualItems().map((virtualRow) => {
+            const ticker = searchTickers[virtualRow.index];
+            const percentChange = Number(ticker.changePercent24Hr);
+            const isGrowth = percentChange === Math.abs(percentChange);
+            const isSelected = !!tickers.find(
+              (savedTicker) => savedTicker.id === ticker.id
+            );
+
+            return (
+              <ListItem
+                key={ticker.id}
+                sx={{
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                  height: `${virtualRow.size}px`,
+                  transform: `translateY(${virtualRow.start}px)`,
+                  "&:hover": { backgroundColor: "rgba(0, 0, 0, 0.04)" },
+                  paddingLeft: 0
+                }}
               >
-                <ListItemText>
-                  <Typography
-                    component="h3"
-                    fontWeight="bold"
-                    textTransform="uppercase"
-                  >
-                    {ticker.symbol}
-                  </Typography>
-                  <Typography component="h4" color="textDisabled">
-                    {ticker.name}
-                  </Typography>
-                </ListItemText>
-                <Stack alignItems="flex-end">
-                  <Typography component="span" fontWeight="bold">
-                    {Number(ticker.priceUsd).toFixed(2)}
-                  </Typography>
-                  <Typography
-                    component="span"
-                    color={isGrowth ? "#65c467" : "#eb4e3d"}
-                    fontWeight="bold"
-                  >
-                    {`${Number(ticker.changePercent24Hr).toFixed(3)}%`}
-                  </Typography>
-                </Stack>
-              </Box>
-            </ListItem>
-          );
-        })}
-      </List>
-    </div>
+                <ListItemIcon>
+                  {isSelected ? (
+                    <IconButton
+                      onClick={() =>
+                        setTickers((prevTickers) =>
+                          prevTickers.filter(
+                            (savedTicker) => savedTicker.id !== ticker.id
+                          )
+                        )
+                      }
+                      sx={{
+                        paddingLeft: 0,
+                        "&:hover": { backgroundColor: "transparent" }
+                      }}
+                    >
+                      <CheckCircleIcon color="primary" fontSize="large" />
+                    </IconButton>
+                  ) : (
+                    <IconButton
+                      onClick={() =>
+                        setTickers((prevTickers) => [
+                          ...prevTickers,
+                          { id: ticker.id, value: ticker.symbol }
+                        ])
+                      }
+                      sx={{
+                        paddingLeft: 0,
+                        "&:hover": { backgroundColor: "transparent" }
+                      }}
+                    >
+                      <ControlPointIcon color="primary" fontSize="large" />
+                    </IconButton>
+                  )}
+                </ListItemIcon>
+                <Box
+                  display="flex"
+                  alignItems="center"
+                  sx={{ width: "100%" }}
+                  onClick={() =>
+                    selectTicker({ id: ticker.id, value: ticker.symbol })
+                  }
+                >
+                  <ListItemText>
+                    <Typography
+                      component="h3"
+                      fontWeight="bold"
+                      textTransform="uppercase"
+                    >
+                      {ticker.symbol}
+                    </Typography>
+                    <Typography component="h4" color="textDisabled">
+                      {ticker.name}
+                    </Typography>
+                  </ListItemText>
+                  <Stack alignItems="flex-end">
+                    <Typography component="span" fontWeight="bold">
+                      {Number(ticker.priceUsd).toFixed(2)}
+                    </Typography>
+                    <Typography
+                      component="span"
+                      color={isGrowth ? "#65c467" : "#eb4e3d"}
+                      fontWeight="bold"
+                    >
+                      {`${Number(ticker.changePercent24Hr).toFixed(3)}%`}
+                    </Typography>
+                  </Stack>
+                </Box>
+              </ListItem>
+            );
+          })}
+        </List>
+      )}
+    </Box>
   );
 };
 
