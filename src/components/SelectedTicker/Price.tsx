1935ed80328ae1f56d94fbb67cdb9e57ec637664
Andrii Zaitsev <andreyzaitsev31@gmail.com>
2025-01-06 22:23:14 +0200

    Make Price component render history interval for price
    - I also removed unnecessary file _state_

--- src/components/SelectedTicker/Price.tsx
+++ src/components/SelectedTicker/Price.tsx
@@ -1,20 +1,31 @@
-import { Box, Typography } from "@mui/material";
+import { useRecoilValue } from "recoil";
+import { Stack, Typography } from "@mui/material";
+import { historyIntervalState } from "@/state";
+import { HistoryInterval } from "@/commonTypes";
 
 export type PriceProps = {
   price: number;
 };
 
 const Price = ({ price }: PriceProps) => {
+  const { label: intervalLabel, value } = useRecoilValue(historyIntervalState);
+  const isToday = value === HistoryInterval.Day;
   return (
-    <Box>
-      <Typography component="span" fontSize="1.5rem">
-        {Number(price).toLocaleString("en-US", {
-          style: "currency",
-          currency: "USD",
-          minimumFractionDigits: 2
-        })}
-      </Typography>
-    </Box>
+    <Stack alignItems="flex-start">
+      <Stack direction="row" justifyContent="space-between" alignItems="center">
+        <Typography component="span" fontSize="1.5rem">
+          {Number(price).toLocaleString("en-US", {
+            style: "currency",
+            currency: "USD",
+            minimumFractionDigits: 2
+          })}
+        </Typography>
+        <Typography ml="1.3rem">-1000</Typography>
+      </Stack>
+      {!isToday && (
+        <Typography>{`Past ${intervalLabel.toLowerCase()}`}</Typography>
+      )}
+    </Stack>
   );
 };
 
