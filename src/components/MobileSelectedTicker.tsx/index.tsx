4d7ae16adb69599817919330fe737ce2c2865063
Andrii Zaitsev <andreyzaitsev31@gmail.com>
2025-01-27 01:22:12 +0200

    Add price to mobile ticker view

--- src/components/MobileSelectedTicker.tsx/index.tsx
+++ src/components/MobileSelectedTicker.tsx/index.tsx
@@ -1,9 +1,18 @@
-import { useSetRecoilState } from "recoil";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { useQuery } from "@tanstack/react-query";
 import CloseIcon from "@mui/icons-material/Close";
-import { Stack, Typography, IconButton } from "@mui/material";
-import { displayMobileSelectedTickerState } from "@/state";
+import { Stack, Box, Typography, IconButton } from "@mui/material";
+import { selectedTickerState, displayMobileSelectedTickerState } from "@/state";
+import { getTicker } from "@/api";
+import Price from "@/components/SelectedTicker/Price";
 
 const MobileSelectedTicker = () => {
+  const selectedTicker = useRecoilValue(selectedTickerState);
+  const { data, status } = useQuery({
+    queryKey: ["tickerId", selectedTicker.id],
+    queryFn: () => getTicker(selectedTicker.id)
+  });
+
   const setDisplayMobileSelectedTicker = useSetRecoilState(
     displayMobileSelectedTickerState
   );
@@ -12,16 +21,19 @@ const MobileSelectedTicker = () => {
       <Stack direction="row" justifyContent="space-between">
         <Stack direction="row" alignItems="center">
           <Typography component="h1" fontSize="2rem" fontWeight="bold">
-            XLM
+            {selectedTicker.value}
           </Typography>
           <Typography component="h2" color="textDisabled" ml="1rem">
-            Bitcoin
+            {data.name}
           </Typography>
         </Stack>
         <IconButton onClick={() => setDisplayMobileSelectedTicker(false)}>
           <CloseIcon />
         </IconButton>
       </Stack>
+      <Box borderTop="1px solid grey" borderBottom="1px solid grey">
+        <Price price={Number(data.priceUsd)} />
+      </Box>
     </Stack>
   );
 };
