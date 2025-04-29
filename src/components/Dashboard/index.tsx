bda10a0825478109a011bdceeb879e69a7fa887d
Andrii Zaitsev <andreyzaitsev31@gmail.com>
2025-04-29 12:16:23 +0300

    Implement logo

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,17 +1,22 @@
 import MarketTables from "@/components/MarketTables";
 import MarketSentimentCard from "@/components/MarketSentimentCard";
 import MarketOverviewCard from "@/components/MarketOverviewCard";
+import { BarChartIcon } from "@radix-ui/react-icons";
+import { Container, Flex } from "@radix-ui/themes";
 
 const Dashboard = () => {
   return (
-    <div>
-      <div>logo</div>
+    <Container>
+      <Flex align="center" gap="2">
+        <BarChartIcon width={40} height={40} color="#22c55e" />
+        <h1>CryptoLight</h1>
+      </Flex>
       <div>
         <MarketSentimentCard />
         <MarketOverviewCard />
       </div>
       <MarketTables />
-    </div>
+    </Container>
   );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import { RecoilRoot } from "recoil";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App.tsx";
+import { Theme } from "@radix-ui/themes";
 
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import "@radix-ui/themes/styles.css";
 import "./index.css";
 
 const queryClient = new QueryClient();
@@ -16,7 +18,9 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <RecoilRoot>
-        <App />
+        <Theme>
+          <App />
+        </Theme>
       </RecoilRoot>
     </QueryClientProvider>
   </StrictMode>
