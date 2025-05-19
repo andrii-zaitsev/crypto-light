2b4d38d1e170df80a944552cf80f502e88a25b81
Andrii Zaitsev <andreyzaitsev31@gmail.com>
2025-05-19 12:45:35 +0300

    Add loading text to All markets table

--- src/components/AllMarketsTable/index.tsx
+++ src/components/AllMarketsTable/index.tsx
@@ -30,85 +30,89 @@ const AllMarketsTable = ({
       <Text as="p" color="gray" mb="0.5rem">
         Market data for Top 100 cryptocurrencies
       </Text>
-      <Table.Root>
-        <Table.Header>
-          <Table.Row>
-            <Table.ColumnHeaderCell>Rank</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Name</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Price</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>24h Change</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Market Cap</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Volume(24h)</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Original source</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell />
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {coinsList.map(
-            ({
-              rank,
-              name,
-              priceUsd,
-              changePercent24Hr,
-              marketCapUsd,
-              volumeUsd24Hr,
-              explorer
-            }) => (
-              <Table.Row key={name}>
-                <Table.Cell>{rank}</Table.Cell>
-                <Table.Cell>{name}</Table.Cell>
-                <Table.Cell>
-                  {new Intl.NumberFormat("en-US", {
-                    style: "currency",
-                    currency: "USD"
-                  }).format(Number(priceUsd))}
-                </Table.Cell>
-                <Table.Cell>
-                  <DayPriceChangeText value={changePercent24Hr} />
-                </Table.Cell>
-                <Table.Cell>
-                  {new Intl.NumberFormat("en-US", {
-                    style: "currency",
-                    currency: "USD",
-                    notation: "compact"
-                  }).format(Number(marketCapUsd))}
-                </Table.Cell>
-                <Table.Cell>
-                  {new Intl.NumberFormat("en-US", {
-                    style: "currency",
-                    currency: "USD",
-                    notation: "compact"
-                  }).format(Number(volumeUsd24Hr))}
-                </Table.Cell>
-                <Table.Cell>
-                  <Flex align="center">
-                    <Link
-                      href={explorer || "https://www.coincap.io/"}
-                      target="_blank"
-                    >
-                      <Flex align="center">
-                        <Text>See more</Text>
-                        <ArrowTopRightIcon />
-                      </Flex>
-                    </Link>
-                  </Flex>
-                </Table.Cell>
-                <Table.Cell>
-                  {savedCoins.includes(name) ? (
-                    <Text style={{ fontStyle: "italic" }}>
-                      Added to watchlist
-                    </Text>
-                  ) : (
-                    <Button variant="ghost" onClick={() => saveCoin(name)}>
-                      Add to watchlist
-                    </Button>
-                  )}
-                </Table.Cell>
-              </Table.Row>
-            )
-          )}
-        </Table.Body>
-      </Table.Root>
+      {coinsList.length > 0 ? (
+        <Table.Root>
+          <Table.Header>
+            <Table.Row>
+              <Table.ColumnHeaderCell>Rank</Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell>Name</Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell>Price</Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell>24h Change</Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell>Market Cap</Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell>Volume(24h)</Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell>Original source</Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell />
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {coinsList.map(
+              ({
+                rank,
+                name,
+                priceUsd,
+                changePercent24Hr,
+                marketCapUsd,
+                volumeUsd24Hr,
+                explorer
+              }) => (
+                <Table.Row key={name}>
+                  <Table.Cell>{rank}</Table.Cell>
+                  <Table.Cell>{name}</Table.Cell>
+                  <Table.Cell>
+                    {new Intl.NumberFormat("en-US", {
+                      style: "currency",
+                      currency: "USD"
+                    }).format(Number(priceUsd))}
+                  </Table.Cell>
+                  <Table.Cell>
+                    <DayPriceChangeText value={changePercent24Hr} />
+                  </Table.Cell>
+                  <Table.Cell>
+                    {new Intl.NumberFormat("en-US", {
+                      style: "currency",
+                      currency: "USD",
+                      notation: "compact"
+                    }).format(Number(marketCapUsd))}
+                  </Table.Cell>
+                  <Table.Cell>
+                    {new Intl.NumberFormat("en-US", {
+                      style: "currency",
+                      currency: "USD",
+                      notation: "compact"
+                    }).format(Number(volumeUsd24Hr))}
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Flex align="center">
+                      <Link
+                        href={explorer || "https://www.coincap.io/"}
+                        target="_blank"
+                      >
+                        <Flex align="center">
+                          <Text>See more</Text>
+                          <ArrowTopRightIcon />
+                        </Flex>
+                      </Link>
+                    </Flex>
+                  </Table.Cell>
+                  <Table.Cell>
+                    {savedCoins.includes(name) ? (
+                      <Text style={{ fontStyle: "italic" }}>
+                        Added to watchlist
+                      </Text>
+                    ) : (
+                      <Button variant="ghost" onClick={() => saveCoin(name)}>
+                        Add to watchlist
+                      </Button>
+                    )}
+                  </Table.Cell>
+                </Table.Row>
+              )
+            )}
+          </Table.Body>
+        </Table.Root>
+      ) : (
+        <Text color="gray">Loading top cryptocurrencies...</Text>
+      )}
     </Card>
   );
 };
